8604055f66a451fe13c9356e273fe1fa0ac7bd79
Wnifiramos <rami.ounifi@esprit.tn>
2019-01-23 21:36:27 +0100

    profiledone

--- CARPOOLING/Profile/ProfileViewController.swift
+++ CARPOOLING/Profile/ProfileViewController.swift
@@ -10,9 +10,10 @@
 import UIKit
 import Alamofire
 import AlamofireImage
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+let imagePicker = UIImagePickerController()
     @IBOutlet weak var namelbl: UILabel!
-    
+    let urlupload = "http://marwen1994.alwaysdata.net/Carpooling/public/setimg"
     @IBOutlet weak var email: UILabel!
     @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var adress: UILabel!
@@ -32,13 +33,66 @@ email.text = UserDefaults.standard.string(forKey: "email")!
             
             userimg.af_setImage(withURL: URL(string: imgn)!)
         }
-
+imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
   
     @IBAction func uploadpic(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage]
+            as? UIImage else {
+                return
+        }
+        
+        userimg.image = image
+       
+        self.dismiss(animated: true, completion: nil)
+        // self.uploadimage ()
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func uploadimage () {
+       
+        //hud.show(in: self, animated: true)
+        let myUrl =  "http://marwen1994.alwaysdata.net/Carpooling/public/setimg"
+        
+        
+ 
+        var imageData = userimg.image!.pngData()
+        
+     
+        let base64String = imageData?.base64EncodedString(options: .lineLength64Characters)
+        
+        let param = [
+            "id" : id_user,
+            "image":id_user,
+            "image1":base64String!
+            ] as [String : Any]
+        Alamofire.request(myUrl,method: .post, parameters: param).responseJSON
+            {
+               
+                response in
+                 print(base64String)
+                print("done")
+                
+                
+        }
+    }
+    
+    
+    @IBAction func newuploadbtn(_ sender: UIButton) {
+        self.uploadimage ()
+    }
 }
