5f9434077b9ebef7c246194f1c58a51d2de9cc00
Wnifiramos <rami.ounifi@esprit.tn>
2019-01-24 15:41:43 +0100

    map with user location

--- CARPOOLING/Annonces/AnnoncefirstViewController.swift
+++ CARPOOLING/Annonces/AnnoncefirstViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
-
-class AnnoncefirstViewController: UIViewController {
-
+import CoreLocation
+import MapKit
+class AnnoncefirstViewController: UIViewController,CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lang = ""
+    var lat = ""
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
-    */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01,longitudeDelta: 0.01 )
+        let mylocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude,location.coordinate.longitude )
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: mylocation, span: span)
+        map.setRegion(region, animated: true)
+        self.lang = "" + "\(mylocation.longitude) "
+        self.lat = "" + "\(mylocation.latitude) "
+        print(mylocation.latitude)
+        self.map.showsUserLocation = true;
+        //checkifexist()
+    }
 
+    @IBAction func nextbtnclicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "steptwo", sender: self)
+    }
+    @IBAction func cancelbtnclicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
 }
